d2d4134849347a00473b03214596c8f20d4881fc
user <user@WINDOWS-5AN5BRH>
2024-12-10 20:42:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Масив чисел
+        int[] numbers = { 2, 12, 14, 18, 25, 15 };
+
+        // Обчислення середнього арифметичного
+        double average = numbers.Average();
+
+        // Виведення результату
+        Console.WriteLine($"Середнє арифметичне чисел: {average}");
+    }
+}
+
+
